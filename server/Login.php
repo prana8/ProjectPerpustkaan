6fe6515e21eedc4e557d2f7ba8479a1b34ac924d
Ryan Prana <ryanprana555@gmail.com>
2023-07-03 21:53:49 +0800

    fix login php

--- server/Login.php
+++ server/Login.php
@@ -3,6 +3,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Content-Type: application/json; charset=UTF-8");
 
+
+include './connection.php';
 // Mendapatkan data dari request
 $data = json_decode(file_get_contents("php://input"));
 
