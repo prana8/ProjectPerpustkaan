db8006f71c44db84209ff80702d523d75c554b48
Ryan Prana <ryanprana555@gmail.com>
2023-07-05 02:32:26 +0800

    Edit Timekeeper & book.php

--- server/Book.php
+++ server/Book.php
@@ -3,27 +3,20 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "peminjaman_buku";
 
-// Create a connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+include "./connection.php";
 
-// Check the connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
 
-$method = $_SERVER['REQUEST_METHOD'];
+
+try{
+  $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
   case "GET":
     $sql = "SELECT book_pdf FROM buku WHERE id = 1";
     $result = $conn->query($sql);
-    if ($result && $result->num_rows > 0) {
-      $row = $result->fetch_assoc();
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+    if ($row) {
       $pdfData = $row['book_pdf'];
 
       // Set the appropriate headers
@@ -37,5 +30,7 @@ switch ($method) {
 
     break;
 }
+} catch (PDOException $e) {
+  echo "Koneksi gagal", $e->getMessage();
+}
 
-$conn->close();
--- server/timekeeper.php
+++ server/timekeeper.php
@@ -2,26 +2,29 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: PATCH");
 
 include "./connection.php";
+$data = json_decode(file_get_contents("php://input"));
+
+if ($_SERVER['REQUEST_METHOD'] !== 'PATCH') {
+    echo json_encode(array("message" => "Metode permintaan tidak valid."));
+    exit;
+}
 
 try {
-
-    $id_user = $data->id;
-    $addSecond = $data->addSecond;
-
-
-    $sql = "UPDATE subscription SET timekeeper = DATE_ADD(timekeeper, INTERVAL :addSecond SECOND) WHERE id_user = :id_user";
+    
+    $sql = "UPDATE subscription SET timekeeper=DATE_ADD(timekeeper, INTERVAL :addSecond SECOND) WHERE id_user=:id";
     $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':addSecond', $addSecond, PDO::PARAM_INT);
-    $stmt->bindParam(':id_user', $id_user, PDO::PARAM_INT);
-
-
+    $stmt->bindParam(':addSecond', $data->addSecond, PDO::PARAM_INT);
+    $stmt->bindParam(':id', $data->id, PDO::PARAM_INT);
     $stmt->execute();
 
-    echo json_encode(array("message" => "Pembaruan berhasil."));
+    echo "Pembaruan berhasil";
 } catch (PDOException $e) {
-    echo json_encode(array("message" => "Error: " . $e->getMessage()));
+    echo "Pembaruan gagal: " . $e->getMessage();
 }
+
+$conn = null;
 
 ?>
