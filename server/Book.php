6d1cea17a4ad2ac4d43544817ad9531be36d9b51
ramsteel <ramajg8800@gmail.com>
2023-07-07 01:11:08 +0800

    update subscription

--- server/Book.php
+++ server/Book.php
@@ -10,32 +10,32 @@ $path = $_SERVER['PATH_INFO'];
 $id = substr($path, 1);
 
 
-try{
+try {
   $method = $_SERVER['REQUEST_METHOD'];
 
-switch ($method) {
-  case "GET":
-    $stmt = $conn->prepare("SELECT book_pdf FROM buku WHERE id = :id");
-    $stmt->bindParam(":id", $id);
-    $stmt->execute();
+  switch ($method) {
+    case "GET":
+      $stmt = $conn->prepare("SELECT book_pdf FROM buku WHERE id = :id");
+      $stmt->bindParam(":id", $id);
+      $stmt->execute();
 
-    if ($stmt->rowCount() > 0) {
-      // Pengguna ditemukan, mengambil data pengguna dari hasil query
-      $book = $stmt->fetch(PDO::FETCH_ASSOC);
-      $pdfData = $book['book_pdf'];
+      if ($stmt->rowCount() > 0) {
+        // Pengguna ditemukan, mengambil data pengguna dari hasil query
+        $book = $stmt->fetch(PDO::FETCH_ASSOC);
+        $pdfData = $book['book_pdf'];
 
-      header("Content-type: application/pdf");
-      header("Content-Disposition: inline; filename=" . $pdfData . ".pdf");
+        header("Content-type: application/pdf");
+        header("Content-Disposition: inline; filename=" . $pdfData . ".pdf");
 
-      echo $pdfData;
-    } else {
-      // Pengguna tidak ditemukan, mengirim response status error
-      $response = array("status" => "error", "message" => "Error fetch PDF");
-      echo json_encode($response);
-    }
+        echo $pdfData;
+      } else {
+        // Pengguna tidak ditemukan, mengirim response status error
+        $response = array("status" => "error", "message" => "Error fetch PDF");
+        echo json_encode($response);
+      }
 
-    break;
-}
+      break;
+  }
 } catch (PDOException $e) {
   echo "Koneksi gagal", $e->getMessage();
 }
--- server/Subscription.php
+++ server/Subscription.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -8,23 +10,21 @@ include "./Connection.php";
 
 $data = json_decode(file_get_contents("php://input"));
 
-try {
+$method = $_SERVER['REQUEST_METHOD'];
 
-    $path = $_SERVER['PATH_INFO'];
-    $id = substr($path, 1);
+switch ($method) {
+    case 'PATCH':
+        try {
+            $sql = "UPDATE subscription SET timekeeper = DATE_SUB(timekeeper, INTERVAL :addSecond SECOND) WHERE id_user = :id_user";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':addSecond', $data->subSecond);
+            $stmt->bindParam(':id_user', $data->id);
 
-    $addSecond = $data->addSecond;
+            $stmt->execute();
 
-
-    $sql = "UPDATE subscription SET timekeeper = DATE_ADD(timekeeper, INTERVAL :addSecond SECOND) WHERE id_user = :id_user";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':addSecond', $addSecond, PDO::PARAM_INT);
-    $stmt->bindParam(':id_user', $id, PDO::PARAM_INT);
-
-
-    $stmt->execute();
-
-    echo json_encode(array("message" => "Pembaruan berhasil."));
-} catch (PDOException $e) {
-    echo json_encode(array("message" => "Error: " . $e->getMessage()));
+            echo json_encode(array("message" => "Pembaruan berhasil.", "id" => $data->id));
+        } catch (PDOException $e) {
+            echo json_encode(array("message" => "Error: " . $e->getMessage()));
+        }
+        break;
 }
